2e7e5f1992f738be69fb2ce7013b58c3a0f05e5a
johnatha33n <johnatha33n@c0cd98e0-b940-11de-9846-1b058f2a8eb1>
2009-12-03 18:33:34 +0000

    finish player status class

--- Submissions/Phase III/Game/status.py
+++ Submissions/Phase III/Game/status.py
@@ -7,10 +7,11 @@ class Status(object):
     def __init__ (self, screen, fontPath):
         
         self.myScreen = screen 
-        self.font = pygame.font.Font(fontPath, 30)
-
-        self.myFont = fontPath
-
+        self.myFont = pygame.font.Font(fontPath,30)
+        self.myFontNone = pygame.font.Font(None, 30)
+       
+        self.font = self.myFont 
+        
         self.hCord_x = 5
         self.hCord_y = 5
         
@@ -45,91 +46,70 @@ class Status(object):
         self.lAmber = (225,194,91)
         self.lRed   = (155, 20,20)
         
+        self.currentWeapon = ""
         self.hpBarColor = self.lGreen
         
-    def upDate(self, healthPoints, score, lives):
+    def upDate(self, healthPoints, score, lives, currentWeap):
         
         self.currentHP = healthPoints
+        self.currentWeapon = currentWeap
         #currentWeapon.set_colorkey((255,255,255), RLEACCEL)
-
         
-     
-        
-      
-       
-            
-
         if healthPoints != self.previousHP:
              # HP Label
             self.healthPointTxt = self.font.render("HP:", 1, self.labelColor)
+            self.font = self.myFontNone
             self.hpNumTxt = self.font.render(str(healthPoints), 1, self.labelColor)
-           
-            if healthPoints <= 70:
-                self.hpBarColor = self.lAmber
-                self.redFlag = False
-            if healthPoints <= 40:
-                self.hpBarColor = self.lRed
-                self.redFlag = True
-            else:
-                self.hpBarColor = self.lGreen
-                self.redFlag = False  
+            self.font = self.myFont
        
         if score != self.previousScore:
             # Score
             self.scoreTxt = self.font.render("Score: ", 1, self.labelColor)
-            self.font = pygame.font.Font(None, 30)
+            self.font = self.myFontNone
             self.scoreNumTxt = self.font.render(str(score).rjust(7), 1, self.labelColor)
-            
+            self.font = self.myFont
         
         if lives != self.previousLives:
             # Lives
-            self.font = pygame.font.Font(self.myFont, 30)
             self.liveTxt  = self.font.render("Lives: ", 1, self.labelColor)        
+            self.font = self.myFontNone
             self.lifeNumTxt = self.font.render(str(lives).rjust(2), 1, self.labelColor)
+            self.font = self.myFont
         
-          
-        
-
-       # self.myScreen.blit(currentWeapon, (self.cw_x, self.cw_y))
-        
-     
-        
-
-
-
-        
-        # Note: HealthPoints Max out at 100 and Min at 0
-        # rect(screen, (color), (x,y,w,h)
-  
-      
-  
 
         self.previousHP = healthPoints
         self.previousScore = score
         self.previousLives = lives
 
     def render(self):
-        print(str(self.jCounter)) 
         if self.redFlag:
             if self.jCounter >= 7:
                 self.jCounter = 0
             else:
-                self.jCounter += .5
-                
+                self.jCounter += .5 
+                        
             offSet = math.floor(15 * math.sin(self.jCounter))
-            self.hpBarColor = (155 + offSet,20 + offSet,20 + offSet)
-            
+            self.lRed = (155 + offSet,20 + offSet,20 + offSet)
         
-            
+        if self.currentHP <= 70:
+            self.hpBarColor = self.lAmber
+            self.redFlag = False
+            if self.currentHP <= 40:
+                self.hpBarColor = self.lRed
+            self.redFlag = True
+        else:
+            self.hpBarColor = self.lGreen
+            self.redFlag = False  
+        
         # HP
         self.myScreen.blit(self.healthPointTxt, (self.hCord_x, self.hCord_y))
-        self.myScreen.blit(self.hpNumTxt, (self.hCord_x + 140, self.hCord_y))
+        self.myScreen.blit(self.hpNumTxt, (self.hCord_x + 140, self.hCord_y + 3))
         # Score
         self.myScreen.blit(self.scoreTxt, (self.sCord_x, self.sCord_y))
-        self.myScreen.blit(self.scoreNumTxt, (self.sCord_x + 60, self.sCord_y + 4))
+        self.myScreen.blit(self.scoreNumTxt, (self.sCord_x + 60, self.sCord_y + 3))
         # Lifes
         self.myScreen.blit(self.liveTxt, (self.lCord_x, self.lCord_y))
-        self.myScreen.blit(self.lifeNumTxt, (self.lCord_x + 50, self.lCord_y + 4))
+        self.myScreen.blit(self.lifeNumTxt, (self.lCord_x + 50, self.lCord_y + 3))
         
         pygame.draw.rect(self.myScreen, self.hpBarColor,(self.hBar_x + 35, self.hBar_y, self.currentHP, self.hBar_height))
         
@@ -139,7 +119,9 @@ class Status(object):
         pygame.draw.line(self.myScreen, self.outLineRectColor, (self.hBar_x + 34, self.hBar_y + self.hBar_height),(self.hBar_x + 136, self.hBar_y + self.hBar_height))
         pygame.draw.line(self.myScreen, self.outLineRectColor, (self.hBar_x + 136, self.hBar_y),(self.hBar_x + 136, self.hBar_y + self.hBar_height))
 
-
+        
+        self.myScreen.blit(self.currentWeapon, (self.cw_x, self.cw_y))
+        # fream for currentweapon pic
         currentWeaponFrameColor = (232, 132, 24)
         pygame.draw.rect(self.myScreen, currentWeaponFrameColor, (self.cw_x, self.cw_y, 13, 3))
         pygame.draw.rect(self.myScreen, currentWeaponFrameColor, (self.cw_x, self.cw_y, 3, 13))
